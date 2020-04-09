1d506ba48df51daeecee58a19fcb40fe123caa35
koki <kokitan_73@icloud.com>
2020-04-10 01:20:02 +0900

    下層service/contact作成

--- js/header.js
+++ js/header.js
@@ -1,61 +1,56 @@
 $(function () {
 
-  $(window).on('load resize', function () { //縦幅、横幅変わるたびに変数をリサイズ
+  var $headerTop = $('#header').offset().top; //headerのウインドウ高さ 全て
 
-    //-------------リサイズ要な変数たち----------------
-    var $headerTop = $('#header').offset().top; //headerのウインドウ高さ
-    var $workHeight = $('#work').offset().top - 81; //workのウインドウ高さ
-    var $serviceHeight = $('#service').offset().top - 81; //serviceのウインドウ高さ
-    var $aboutHeight = $('#about').offset().top - 81; //aboutのウインドウ高さ
-    var $contactHeight = $('#contact').offset().top - 81; //contactのウインドウ高さ
-    //-------------リサイズ要な変数たち----------------
+  if (document.URL.match("/index.html")) { //topページのみ
 
-
-    //PC/SP共通
-    //header固定に関する処理
-    $(window).on('scroll', function () {
-      if ($(this).scrollTop() > $headerTop) { //ウインドウスクロールがheaderまでのウインドウ高さを超えたらheader固定
-        $('.pc-header').addClass('header_fixed');
-      } else { //ウインドウスクロールがheaderまでのウインドウ高さ+headerの高さを超えないとheader固定しない
-        $('.pc-header').removeClass('header_fixed');
-      }
-    });
+    $(window).on('scroll', function () { //topページのみ
     
-    //PCのヘッダー
-    //スクロールで対象のナビリンクに色つけ
-
-    $(window).on('scroll', function () {
-      
+      //-------------topページでのみ必要な変数たち----------------
+      var $workHeight = $('#work').offset().top - 81; //workのウインドウ高さ topのみ
+      var $serviceHeight = $('#service').offset().top - 81; //serviceのウインドウ高さ topのみ
+      var $aboutHeight = $('#about').offset().top - 81; //aboutのウインドウ高さ topのみ
+      var $contactHeight = $('#contact').offset().top - 81; //contactのウインドウ高さ topのみ
+      //-------------topページでのみ必要な変数たち----------------
+        
       var $windowTop = $(this).scrollTop(); //現在のスクロール位置を取得
-      
-      if ($windowTop > $workHeight && $windowTop < $serviceHeight) { //workのセクションでworkに色つけ
-        $('.header-list li').removeClass('scroll_color');
-        $('.header-list li:nth-child(1)').addClass('scroll_color');
-      } else if ($windowTop > $serviceHeight && $windowTop < $aboutHeight) { //serviceのセクションでserviceに色つけ
+  
+      //スクロールで対象のナビリンクに色つけ
+      if ($windowTop > $workHeight && $windowTop <= $serviceHeight) { //workのセクションでworkに色つけ
         $('.header-list li').removeClass('scroll_color');
         $('.header-list li:nth-child(2)').addClass('scroll_color');
-      } else if ($windowTop > $aboutHeight && $windowTop < $contactHeight) { //aboutのセクションでaboutに色つけ
+      } else if ($windowTop > $serviceHeight && $windowTop <= $aboutHeight) { //serviceのセクションでserviceに色つけ
         $('.header-list li').removeClass('scroll_color');
         $('.header-list li:nth-child(3)').addClass('scroll_color');
-      } else if ($windowTop > $contactHeight) { //contantのセクションでcontactに色つけ
+      } else if ($windowTop > $aboutHeight && $windowTop <= $contactHeight) { //aboutのセクションでaboutに色つけ
         $('.header-list li').removeClass('scroll_color');
         $('.header-list li:nth-child(4)').addClass('scroll_color');
+      } else if ($windowTop > $contactHeight) { //contantのセクションでcontactに色つけ
+        $('.header-list li').removeClass('scroll_color');
+        $('.header-list li:nth-child(5)').addClass('scroll_color');
       } else { //上記以外は色なし
         $('.header-list li').removeClass('scroll_color');
       }
-      
-      //SPのヘッダー
-      //トグル表示処理
-      if ($windowTop > $headerTop) { //スクロール高さがheader高さ超えたらトグル表示
-        $('#toggle').fadeIn();
+    });
+  } //topページのみ必要な処理終了
+
+    $(window).on('scroll resize', function () {
+        
+      var $windowTop = $(this).scrollTop(); //現在のスクロール位置を取得
+
+      console.log($headerTop);
+  
+      //header固定に関する処理、トグルの非表示処理
+      if ($windowTop > $headerTop) { //ウインドウスクロールがheaderまでのウインドウ高さを超えたら
+        $('.pc-header').addClass('header_fixed'); //header固定
+        $('#toggle').fadeIn(); //トグルフェードイン
         //console.log('表示');
-      } else { //超えなきゃ非表示
-        $('#toggle').fadeOut();
+      } else { //ウインドウスクロールがheaderまでのウインドウ高さ+headerの高さを超えない
+        $('.pc-header').removeClass('header_fixed'); //header固定しない
+        $('#toggle').fadeOut(); //トグルフェードアウト
         //console.log('非表示');
       }
-      
     });
-  });
   
   
   function spHeader() { //トグルの変形処理及びSPリンクのスライドインを関数化
